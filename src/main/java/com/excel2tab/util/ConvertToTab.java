392d033f7486084192b029e332d45a8bfd321580
Mashariq Khan <mashariq@protonmail.com>
2016-04-10 13:50:56 -0400

    dev commit

--- src/main/java/com/excel2tab/util/ConvertToTab.java
+++ src/main/java/com/excel2tab/util/ConvertToTab.java
@@ -1,5 +1,20 @@
 package com.excel2tab.util;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ConvertToTab {
 
+	public static void main(String[] args){
+
+		Properties props = new Properties();
+		try{
+			InputStream resourceStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("project.properties");
+			props.load(resourceStream);
+			System.out.println("booyah bru");
+			System.out.println(props.getProperty("hola"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
