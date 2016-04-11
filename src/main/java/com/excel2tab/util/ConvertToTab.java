38e7338eaac0db8234586e80d5bfaa7223280750
Mashariq Khan <mashariq@protonmail.com>
2016-04-11 12:16:39 -0400

    dev commit

--- src/main/java/com/excel2tab/util/ConvertToTab.java
+++ src/main/java/com/excel2tab/util/ConvertToTab.java
@@ -1,20 +1,33 @@
 package com.excel2tab.util;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ConvertToTab {
 
-	public static void main(String[] args){
+	public static void main (String[] args) throws Exception {
 
 		Properties props = new Properties();
+		File excelFile =null;
 		try{
 			InputStream resourceStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("project.properties");
+			System.out.println("resourceStream is "+resourceStream);
 			props.load(resourceStream);
-			System.out.println("booyah bru");
-			System.out.println(props.getProperty("hola"));
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		
+		try {
+			excelFile = new File(props.getProperty("inputExcel"));
+			System.out.println("Input dir is "+props.getProperty("inputExcel"));
+			System.out.println("wbStream is "+excelFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Workbook wb = new XSSFWorkbook(excelFile);
 	}
 }
